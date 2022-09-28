2f216a243203b2f3ecd3dea9f1bdfd912d2f4992
Larissa Tonetto <larigottschall@hotmail.com>
2022-09-27 22:02:00 -0300

    devicefarm

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,33 +1,50 @@
 //const {join} = require('path')
 const allure = require('allure-commandline')
+const video = require('wdio-video-reporter');
 
 exports.config = {
-  hostname: 'localhost',
-  port: 4723,
-  path: '/wd/hub',
-  services: ['appium'],
+  //hostname: 'localhost',
+  //port: 4723,
+  //path: '/wd/hub',
+  user: 'larigottschall_SO9fNG',
+  key: 'DnFJLzqCG84z3z5RxZ9g',
+  //services: ['appium'],
+  service: ['browserstack'],
   specs: [
     './test/specs/**/*.js'
   ],
   framework: 'mocha',
   capabilities: [{
     // "app": join(process.cwd(), '.app/android/woocoom.apk'),
-    "platformName": "Android",
-    "appium:platformVersion": "9.0",
-    "appium:deviceName": "ebac-qe",
-    "appium:automationName": "UiAutomator2",
-    "appium:app": "C:\\Users\\LarissaTonetto\\Repositorio\\testes-mobile-android-2\\app\\android\\woocoom.apk",
-    "appium:appWaitActivity": "com.woocommerce.android.ui.login.LoginActivity"
+    // "platformName": "Android",
+    // "appium:platformVersion": "9.0",
+    // "appium:deviceName": "ebac-qe",
+    // "appium:automationName": "UiAutomator2",
+    // "appium:app": "C:\\Users\\LarissaTonetto\\Repositorio\\testes-mobile-android-2\\app\\android\\woocoom.apk",
+    // "appium:appWaitActivity": "com.woocommerce.android.ui.login.LoginActivity"
+
+    'app': 'bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c',
+    'device': 'Google Pixel 3',
+    'os_version': '9.0',
+    'project': 'Meu primeiro projeto',
+    'build': '1',
+    'name': 'teste_login'
+
+
   }],
   waitforTimeout: 20000,
   mochaOpts: { timeout: 300000 },
   reporters: ['spec',
-  ['allure', {
-    outputDir: 'allure-results',
-    disableWebdriverStepsReporting: false,
-    disableWebdriverScreenshotsReporting: false,
-  }],
-],
+    ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: true,
+    }],
+    [video, {
+      saveAllVideos: true,       // If true, also saves videos for successful test cases
+      videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+    }]
+  ],
   onComplete: function () {
     const reportError = new Error('Could not generate Allure report')
     const generation = allure(['generate', 'allure-results', '--clean'])
