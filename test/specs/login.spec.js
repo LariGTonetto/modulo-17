2fb681baa74a99fc2957576e9f222c8f94970219
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2022-09-27 22:09:50 -0300

    Update login.spec.js

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,7 +1,30 @@
+//describe('Access Admin Panel', () => {
+//    it('Sould login with valid credentials', async () => {
+//        setTimeout (() => {
+//            console.log('abriu')
+//        }, 500000)
+//    });  
+//})
+
+const homeScreen = require("../screens/home.screen")
+const loginScreen = require("../screens/login.screen")
+const myStoreScreen = require("../screens/myStore.screen")
+
+let urlLoja = 'http://lojaebac.ebaconline.art.br/'
+let usuario = 'gerente'
+let senha = 'GD*peToHNJ1#c$sgk08EaYJQ'
+
 describe('Access Admin Panel', () => {
-    it('Sould login with valid credentials', async () => {
-        setTimeout (() => {
-            console.log('abriu')
-        }, 500000)
-    });  
+    it('shoud login with valid credentials', async () => {
+        await homeScreen.goToLogin()
+        await loginScreen.setStoreAddress(urlLoja)
+        await loginScreen.continue()
+        await loginScreen.continueWithStoreCredentials()
+        await loginScreen.login(usuario, senha)
+        await loginScreen.goToTwoFactorAuth()
+        await loginScreen.twoFactorLogin(senha)
+
+        expect(await myStoreScreen.myStoreLogoIsDisplayed()).toBeTruthy()
+        expect(await myStoreScreen.getStoreName()).toEqual('EBAC - Shop')
+    });
 })
